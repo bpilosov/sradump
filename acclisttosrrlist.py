908b3ef2a0ab62c69bf70115d6a8fa28ef9a50f9
bpilosov <79300734+bpilosov@users.noreply.github.com>
2024-12-24 04:26:30 +0000

    refactor records folder

--- acclisttosrrlist.py
+++ acclisttosrrlist.py
@@ -14,6 +14,9 @@ with open(FULL) as f2:
 # f3.write("".join(matches))
 
 missing = [line for line in full_lines if line not in have_lines]
+f5 = open("missing.txt", "w")
+f5.write("".join(missing))
+
 missingSRR = [line[3:] for line in missing if line[0:3]=="SRR"]
 missingSRR.sort(key=int)
 
