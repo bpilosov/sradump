4b0309a58d3c8c25dc3539d513c7c24aef5de85d
bpilosov <79300734+bpilosov@users.noreply.github.com>
2024-12-29 20:17:30 +0000

    recreate q, multiprocess

--- sradownload.py
+++ sradownload.py
@@ -1,20 +1,23 @@
+import multiprocessing as mp
 import subprocess
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
 import time
 
-CMD = "prefetch -X u SRR{0}"
-MAX_WORKERS = 10
-SUCCESS_LOG = "records/success.log"
-ERROR_LOG = "records/error.log"
-INPUT_FILE = "records/test.csv"
+# -X maximum file size, u=unlimited -O output directory
+CMD = "prefetch -X u -O /mnt/mycephfs/sradownloads SRR{0}"
+MAX_WORKERS = 18
+SUCCESS_LOG = "/mnt/mycephfs/sradownloads/success.log"
+ERROR_LOG = "/mnt/mycephfs/sradownloads/error.log"
+INPUT_FILE = "/mnt/mycephfs/sradownloads/queue.csv"
+MASTER_LIST = "/mnt/mycephfs/sradownloads/missingSRR.csv"
 
 def parallel_prefetch(srr_ids):
     """Execute commands in parallel using ThreadPoolExecutor."""
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+    with ProcessPoolExecutor(max_workers=MAX_WORKERS) as executor:
         # Submit tasks to the executor
         futures = {executor.submit(run_command, srr_id): srr_id for srr_id in srr_ids}
         
-        with open(SUCCESS_LOG, "w") as success_log, open(ERROR_LOG, "w") as error_log:
+        with open(SUCCESS_LOG, "a", buffering=1) as success_log, open(ERROR_LOG, "w") as error_log:
             for future in as_completed(futures):
                 status, message = future.result()
                 if status == "SUCCESS":
@@ -31,8 +34,16 @@ def run_command(srr_id):
     except subprocess.CalledProcessError as e:
         return "ERROR", f"SRR{srr_id}{e.stderr}"
 
+def recreate_queue():
+    with open(MASTER_LIST, "r") as master, open(SUCCESS_LOG, "r") as successlog, open(INPUT_FILE, "w") as queue:
+        missing = [line for line in master if line not in successlog]
+        queue.write("".join(missing))
+        print(f"Queue Restored, {len(missing)} records to be downloaded...")
+
 def main():
     start = time.perf_counter()
+    recreate_queue()
+
     # Read SRR IDs from the input file
     with open(INPUT_FILE, 'r') as file:
         srr_ids = [line for line in file]
